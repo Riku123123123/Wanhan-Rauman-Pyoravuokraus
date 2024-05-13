787cd60b748a0036e3b526bc046f609ac46c9208
Riku123123123 <145870211+Riku123123123@users.noreply.github.com>
2024-05-13 23:43:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function showDetails() {
         section.style.display = 'block';
     });
 }
+
+const express = require('express');
+const app = express();
+
+// Aseta eväste, joka vanhenee 1 tunnin kuluttua
+document.cookie = "kayttaja=arvo; expires=" + new Date(new Date().getTime() + 3600 * 1000).toUTCString() + "; path=/";
+
+
+app.get('/', (req, res) => {
+  res.cookie('kayttaja', 'arvo', { maxAge: 3600 * 1000, httpOnly: true });
+  res.send('Eväste asetettu');
+});
+
+app.listen(3000, () => {
+  console.log('Palvelin käynnissä portissa 3000');
+});
